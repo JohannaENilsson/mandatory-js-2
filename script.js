f119e731a37074526aebfcd0a3d07c4e2d39a330
Johanna Nilsson <nijodotcom@gmail.com>
2019-11-22 16:09:52 +0100

    removed backticks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const gamefield = document.querySelector("#gamefield");
-let resetBtn = document.querySelector("main button");
-let replayBtn = document.querySelector(`#popUp button`);
-let squares = document.querySelectorAll(".square");
-let popUp = document.querySelector(`#containerPopUp`);
-let winP = document.querySelector(`#popUp p`);
+const gamefield = document.querySelector('#gamefield');
+let resetBtn = document.querySelector('main button');
+let replayBtn = document.querySelector('#popUp button');
+let squares = document.querySelectorAll('.square');
+let popUp = document.querySelector('#containerPopUp');
+let winP = document.querySelector('#popUp p');
 
 const winningCombo = [
   [0, 1, 2],
@@ -33,27 +33,27 @@ let OArr = [];
 // PLAY -> Spelet startar
 function play() {
   gameOn = true;
-  // console.log(`play call`);
+  // console.log('play call');
 }
 
 // Sätter dit eventlyssnarna
 function setUp() {
-  resetBtn.style.display = `block`; // Visar reset knappen igen
-  winP.innerHTML = ""; // Vinnartext rensas
+  resetBtn.style.display = 'block'; // Visar reset knappen igen
+  winP.innerHTML = ''; // Vinnartext rensas
   for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener(`click`, oneClick); // Sätter dit eventlyssnare på alla
+    squares[i].addEventListener('click', oneClick); // Sätter dit eventlyssnare på alla
     clickArr = []; // tömmer arrayen
   }
 }
 
 // WITCH PLAYER
-let player = `X`;
+let player = 'X';
 function witchPlayer() {
-  // console.log(`WitchPlayer`);
-  if (player === `X`) {
-    player = `O`;
-  } else if (player === `O`) {
-    player = `X`;
+  // console.log('WitchPlayer');
+  if (player === 'X') {
+    player = 'O';
+  } else if (player === 'O') {
+    player = 'X';
   }
 }
 
@@ -66,24 +66,24 @@ function oneClick(e) {
   
 
   witchPlayer();
-  if (player === "X") {
-    square.style.color = `green`;
-    square.style.backgroundColor = `rgba(245, 245, 245, 0.377)`;
-    square.innerHTML = "X";
+  if (player === 'X') {
+    square.style.color = 'green';
+    square.style.backgroundColor = 'rgba(245, 245, 245, 0.377)';
+    square.innerHTML = 'X';
     xArr.push(square.dataset.id);
     console.log(`X == ${xArr}`);
-    // console.log(`Draw X`);
-  } else if (player === "O") {
-    square.style.color = `red`;
-    square.style.backgroundColor = `rgba(245, 245, 245, 0.377)`;
-    square.innerHTML = "O";
+    // console.log('Draw X');
+  } else if (player === 'O') {
+    square.style.color = 'red';
+    square.style.backgroundColor = 'rgba(245, 245, 245, 0.377)';
+    square.innerHTML = 'O';
     OArr.push(square.dataset.id);
     console.log(`O == ${OArr}`);
-    // console.log(`Draw O`);
+    // console.log('Draw O');
   }
   // if (clickArr.length >= 5) { // Börjar kolla efter vinnare efter 5 click
-    // console.log(`Click function`, gameOn);
-    // console.log("ITS over 5");
+    // console.log('Click function', gameOn);
+    // console.log('ITS over 5');
     // Efter 5 click börja kolla efter vinst
     if (gameOn === false) {
       // om detta händer ska den inte gå vidare
@@ -99,36 +99,36 @@ function oneClick(e) {
     // }
   }
   
-  square.removeEventListener(`click`, oneClick); // plockar bort efter 1 click
+  square.removeEventListener('click', oneClick); // plockar bort efter 1 click
 }
 
 // RESET
 function resetSquares() {
-  popUp.style.display = `none`; // Döljer popupen
+  popUp.style.display = 'none'; // Döljer popupen
   for (let square of squares) {
-    square.innerHTML = ``; // rensar alla rutorna på innehåll
-    square.style.backgroundColor = ` rgba(255, 255, 255, 0.836)`;
+    square.innerHTML = ''; // rensar alla rutorna på innehåll
+    square.style.backgroundColor = ' rgba(255, 255, 255, 0.836)';
     // console.log(square);
   }
   setUp(); // sätter dit eventlyssnarna
   play(); // startar spelet
 
 }
-resetBtn.addEventListener(`click`, resetSquares);
-replayBtn.addEventListener(`click`,resetSquares);
+resetBtn.addEventListener('click', resetSquares);
+replayBtn.addEventListener('click', resetSquares);
 
 // WINNER OF THE GAME
 function isGameOver() {
   // LOOPAR WINNINGCOMBO
   // console.log(winningCombo);
   for (let i = 0; i < winningCombo.length; i++) {
-    // console.log("first" + winningCombo[i]);
+    // console.log('first' + winningCombo[i]);
     let winArr = winningCombo[i];
     for (let j = 0; j < winArr.length; j++) {
-      // console.log("second " + winArr[j]);
+      // console.log('second ' + winArr[j]);
 
       if (xArr.length > OArr.length) {
-        console.log("X against the winner");
+        console.log('X against the winner');
         //JÄMFÖR X mot winning
         for (let k = 0; k < xArr.length; k++) {
           console.log(xArr[i]);
@@ -138,9 +138,9 @@ function isGameOver() {
           }
         }
       }  if (OArr.length > xArr.length) {
-        console.log("O against the winner");
+        console.log('O against the winner');
         for (let l = 0; l < OArr.length; l++) {
-          console.log("O " + OArr[l] + " Winner " + winArr[j]);
+          console.log(`O ${OArr[l]} Winner ${winArr[j]}`);
           if (OArr[l] === winArr[j]) {
             console.log(`X ${OArr[k]} and ${winArr[j]}`);
             return true;
@@ -155,23 +155,23 @@ function isGameOver() {
 // TIE
 function itsATie() {
   gameOn = false;
-  resetBtn.style.display = `none`;
+  resetBtn.style.display = 'none';
   setTimeout(function() {
     // så att o el x hinner ritas ut
-    popUp.style.display = `flex`;
-    winP.textContent = `This round is a tie`; // insert ${player} 
+    popUp.style.display = 'flex';
+    winP.textContent = 'This round is a tie'; // insert ${player} 
   }, 10);
 }
 
 // ENDGAME
 function endGame() {
-  console.log(`End game`);
+  console.log('End game');
   gameOn = false; // spelet är av
   console.log(`${player} won the game`);
-  resetBtn.style.display = `none`;
+  resetBtn.style.display = 'none';
   setTimeout(function() {     
     // så att o el x hinner ritas ut
-    popUp.style.display = `flex`;
+    popUp.style.display = 'flex';
     winP.textContent = `${player} won this round`; // insert ${player}  
   }, 10);
 }
